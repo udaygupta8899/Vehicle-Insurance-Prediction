6970bb4b40ab62bd0e2812e7f087ed5fe6df2ebd
udaygupta8899 <udaygupta8899@gmail.com>
2025-03-08 09:00:05 +0530

    Project Structure ready and venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Uday Kumar Gupta",
+    author_email="udaygupta8899@gmail.com",
+    packages=find_packages()
+)
